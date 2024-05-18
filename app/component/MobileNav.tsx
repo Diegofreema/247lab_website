ecbb6d70f23faddad12fc693a5b45e48517461c3
Diego <diegofreeman78@gmail.com>
2024-05-18 02:26:39 +0100

    ui

--- app/component/MobileNav.tsx
+++ app/component/MobileNav.tsx
@@ -87,8 +87,10 @@ const MobileNav = ({ isOpen, onClose }: Props) => {
       opacity: 1,
       y: 0,
       transition: {
-        duration: 1,
+        duration: 0.5,
         delay: i * 0.1,
+        type: 'spring',
+        damping: 7,
       },
     }),
   };
